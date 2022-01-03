d0830c9995e4b9bebe929dad1b86b0eb62e552d5
Kofktu <kofktu@gmail.com>
2022-01-03 14:00:57 +0900

    When presenter a viewController, pip hidden fix

---
+++ PIPKit/PIPKit.h
@@ -0,0 +1,18 @@
+//
+//  PIPKit.h
+//  PIPKit
+//
+//  Created by Kofktu on 2022/01/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PIPKit.
+FOUNDATION_EXPORT double PIPKitVersionNumber;
+
+//! Project version string for PIPKit.
+FOUNDATION_EXPORT const unsigned char PIPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PIPKit/PublicHeader.h>
+
+
